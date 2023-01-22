39f9c2ad56b8bd310f5b8ec8ea4e4b9979eb3c95
xxoa <117334318+x-xxoa@users.noreply.github.com>
2023-01-21 21:52:45 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,144 @@
+local function test(t,c,w,b,ob,ow,r) --inputtable, currentvalues, inputweights, biases, outbias, outweight, returntable
+    --declare the functions
+    local function calcnode(t,w,b,n) --gets the dot product of the last layer and the weights and then adds the bias to it. t is the last layer, w is weights, b is the bias and n is the node number
+        local current = 1
+        local add = 0
+        repeat
+            add = add + t[current] * w[current]
+            current = current + 1
+        until current == #t + 1
+        return add + b[n]
+    end
+
+    --declare the variables
+    local current = 1
+    local temp = {}
+    local last = {}
+
+    --do the stuff, i mean what else do i put as a comment here. i know im supposed to put comments in my code so its able to be read by people other than whatever higher power there is because of how messy my code gets but sometimes i dont know what to put there like it could be the most mundane thing but tis so unreadable i have to put a comment explaining it.
+    
+    --calculate the input layer
+    repeat
+        temp[current] = calcnode(t,w,b,current)
+        current = current + 1
+    until current == #t + 1
+
+    current = 1
+
+    --copy temp to last
+    repeat
+        last[current] = temp[current]
+        current = current + 1
+    until current == #t + 1
+
+    current = 1
+
+    --calculate the output layer and return it
+    repeat
+        r[current] = calcnode(last,ow,ob,current)
+        current = current + 1
+    until current == #t +1
+
+    current = 1
+
+    --testing code 
+    --[[for i,v in pairs(temp) do
+        print(i,v)
+    end]]--
+end
+
+inp = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+cv = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+iw = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+bi = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+obi = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+owe = {
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0
+}
+
+re = {
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1
+}
+
+test(inp,cv,iw,bi,obi,owe,re)
+for i,v in pairs(re) do
+    print(i,v)
+end
